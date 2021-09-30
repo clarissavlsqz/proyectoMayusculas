376cb44a9ed876b306bff093c93bd1ae34713875
clarissavlsqz <clarissa.a.velasquez@gmai.com>
2021-09-30 13:23:20 -0500

    first commit

---
+++ proyectoMayusculas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proyectoMayusculas
+//
+//  Created by Clarissa Velásquez Magaña on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
