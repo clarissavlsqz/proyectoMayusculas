ed48063b3fc9c3abd7ab753b3efb1205647953c9
clarissavlsqz <clarissa.a.velasquez@gmai.com>
2021-10-04 16:31:36 -0500

    Made buttons rounded

--- proyectoMayusculas/ViewController.swift
+++ proyectoMayusculas/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonRules: UIButton!
+    @IBOutlet weak var buttonPlay: UIButton!
+    @IBOutlet weak var buttonLeaderboard: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonRules.layer.cornerRadius = 5
+        buttonPlay.layer.cornerRadius = 5
+        buttonLeaderboard.layer.cornerRadius = 5
     }
 
 
