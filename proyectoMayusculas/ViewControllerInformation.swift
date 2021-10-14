eab8efbcaa63ed4f005f7ccd127eefc068b5db96
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2021-10-14 14:01:47 -0500

    added information desc

--- proyectoMayusculas/ViewControllerInformation.swift
+++ proyectoMayusculas/ViewControllerInformation.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class ViewControllerInformation: UIViewController {
+    
+    let info1 = "fue desarrollada por estudiantes del Instituto tecnológico y de Estudios Superiores de Monterrey durante el periodo Agosto-Diciembre 2021 como proyecto final para la clase de Proyecto de Desarrollo para Dispositivos Móviles. \n\nDesarrolladores: Ana Lizbeth Zermeño Torres, Clarissa Andrea Velásquez Magaña y Fernando Martínez Ortiz.\n\nMaestra: Mtra. Yolanda Martínez Treviño\n\nMentora de proyecto: Martha Aurelia Pérez Velasco\n\nSe prohíbe el uso de "
+    let info2 = "con fines de lucro. Esta aplicación fue creada con un enfoque a la información y se prohíbe su distribución para otros usos y medios."
+    
+    let boldText = "Mayúsculas "
+    
+    let attrs = [NSAttributedString.Key.font: UIFont.italicSystemFont(ofSize: 12)]
+    
+    
 
+    @IBOutlet weak var labelInfo: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        attributeText()
         // Do any additional setup after loading the view.
     }
     
+    func attributeText() {
+        let attrString = NSMutableAttributedString(string:boldText, attributes:attrs)
+        let attrString2 = NSMutableAttributedString(string:boldText, attributes:attrs)
+        
+        let part1Normal = NSMutableAttributedString(string:info1)
+        let part2Normal = NSMutableAttributedString(string:info2)
+        
+        attrString.append(part1Normal)
+        attrString2.append(part2Normal)
+        attrString.append(attrString2)
+        
+        labelInfo.attributedText = attrString
+        
+    }
 
     /*
     // MARK: - Navigation
