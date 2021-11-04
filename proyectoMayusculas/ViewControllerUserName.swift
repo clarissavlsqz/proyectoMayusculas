2507bb01c1b54fd06e94be673bcfe5c0c77cda8f
AnaZermeno <anazermeno19@gmail.com>
2021-11-03 21:37:11 -0700

    Se agrego código para la vista de User Name

---
+++ proyectoMayusculas/ViewControllerUserName.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerUserName.swift
+//  proyectoMayusculas
+//
+//  Created by Ana Zermeño torres on 03/11/21.
+//
+
+import UIKit
+
+class ViewControllerUserName: UIViewController {
+
+    @IBOutlet weak var lbUserName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
